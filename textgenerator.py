94dbfe76f198ff528ee70446aa341c0138412c25
Tara <tararengan@gmail.com>
2018-07-02 13:01:32 -0500

    Changes

--- textgenerator.py
+++ textgenerator.py
@@ -8,7 +8,7 @@ import numpy as np
 import argparse
 
 
-def read_return_chars(_input_file):
+def read_file_return_text(_input_file):
     """
     :param _input_file:
     :return:  chars in file
@@ -22,18 +22,31 @@ def read_return_chars(_input_file):
 
 
 
-def initialize(_vocab_size, _hidden_layer_size,)
-
-
-
-def main(input_file, batch_size, hidden_layer_size):
+def initialize(_vocab_size, _hidden_layer_size):
     """
-    :param input_file:
-    :param batch_size:
-    :param hidden_layer_size:
+    :param _vocab_size:
+    :param _hidden_layer_size:
     :return:
     """
 
+    W_xh = np.zeros(_hidden_layer_size, _vocab_size)
+    W_hh = np.zeros(_hidden_layer_size, _hidden_layer_size)
+    W_hy = np.zeros(_vocab_size, _hidden_layer_size)
+
+    b = np.zeros(_hidden_layer_size,)
+    b_prime = np.zeros(_vocab_size,)
+
+    return W_xh, W_hh, W_hy, b, b_prime
+
+
+
+def main(_input_file, _batch_size, _hidden_layer_size):
+
+
+    input_text, chars = read_file_return_text(_input_file)
+    vocab_size = len(chars)
+
+    W_xh, W_hh, W_hy, b, b_prime = initialize(vocab_size, _hidden_layer_size, _batch_size)
 
 
 
@@ -55,6 +68,7 @@ if __name__ == 'main':
 
     #parse arguments
     arguments = parse_arguments()
+    main(arguments.input_file, arguments.batch_size, arguments.hidden_layer_size)
 
 
 
