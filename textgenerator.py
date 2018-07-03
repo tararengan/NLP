4bc01881a903ad4115b8d75b913454e02b6e79b3
Tara <tararengan@gmail.com>
2018-07-03 00:45:32 -0500

    changes

--- textgenerator.py
+++ textgenerator.py
@@ -49,8 +49,7 @@ def forward_pass(_input, _hidden, _W_xh, _W_hh, _W_hy, _b, _b_prime):
 
 
 
-def main(_input_file, _batch_size, _hidden_layer_size):
-
+def main(_input_file, _batch_size, _hidden_layer_size, _learning_rate):
 
     input_text, chars = read_file_return_text(_input_file)
     vocab_size = len(chars)
@@ -60,20 +59,36 @@ def main(_input_file, _batch_size, _hidden_layer_size):
 
     W_xh, W_hh, W_hy, b, b_prime = initialize(vocab_size, _hidden_layer_size, _batch_size)
 
-
     cont = True
     hidden_prev = np.zeros(_hidden_layer_size,)
     start_index = 0
+    end_index = start_index+_batch_size
+    d_hprev_Wxh = np.zeros(W_xh.shape)
+    d_hprev_Whh = np.zeros(W_hh.shape)
+    d_hprev_b = np.zeros(b.shape)
 
+    dW_xh = np.zeros(W_xh.shape)
+    dW_hh = np.zeros(W_hh.shape)
+    dW_hy = np.zeros(W_hy.shape)
+    db = np.zeros(b.shape)
+    db_prime = np.zeros(b_prime.shape)
 
-    while cont == True:
+    mW_xh, mW_hh, mW_hy = np.zeros_like(W_xh), np.zeros_like(W_hh), np.zeros_like(W_hy)
+    mb, mb_prime = np.zeros_like(b), np.zeros_like(b_prime)  # memory variables for Adagrad
 
-        input_chars = input_text[start_index: start_index+_batch_size]
+    while cont:
+
+        #next batch
+        input_chars = input_text[start_index: end_index]
+        target_chars = input_text[start_index+1:end_index+1]
+        loss = 0
 
         for i in range(len(input_chars)):
             char = input_chars[i]
+            target_char = target_chars[i]
             x = np.zeros(vocab_size,)
             index = char_index_dict[char]
+            target_index = char_index_dict[target_char]
             x[index] = 1
 
             #forward pass
@@ -81,28 +96,59 @@ def main(_input_file, _batch_size, _hidden_layer_size):
 
             #gradients - back prop
             dy = probs
-            dy[index] += -1
+            dy[target_index] += -1
 
             dW_hy = np.mm(dy, hidden.T)
             db_prime = dy
 
             dh = np.mm(W_hy.T, dy)
 
-            dh_raw = np.dot(dh, 1-np.square(hidden))
+            d_h_hraw = 1-np.square(hidden)
+
+            dh_raw = np.dot(dh, d_h_hraw)
 
             #tier1
-            dW_xh = np.mm(dh_raw, x.T)
-            dW_hh = np.mm(dh_raw, hidden_prev.T)
-            db = dh_raw
+            dW_xh += np.mm(dh_raw, x.T)
+            dW_hh += np.mm(dh_raw, hidden_prev.T)
+            db += dh_raw
 
-            
+            dh_prev = np.mm(W_hh.T, dh_raw)
+
+            #tier2
+            dW_xh += np.mm(np.diag(dh_prev), d_hprev_Wxh)
+            dW_hh += np.mm(np.diag(dh_prev), d_hprev_Whh)
+            db += np.mm(np.diag(dh_prev), d_hprev_b)
 
 
+            #save for next iter
+            d_hprev_Wxh = np.mm(d_h_hraw, x.T)
+            d_hprev_Whh = np.mm(d_h_hraw, hidden_prev.T)
+            d_hprev_b = d_h_hraw
+
+            #update loss
+            loss += -np.log(probs[target_index])
+
+        #update parameters by adagrad update
+        for param, dparam, mem in zip([W_xh, W_hh, W_hy, b, b_prime],
+                                      [dW_xh, dW_hh, dW_hy, db, db_prime],
+                                      [mW_xh, mW_hh, mW_hy, mb, mb_prime]):
+            mem += dparam * dparam
+            param += -_learning_rate * dparam / np.sqrt(mem + 1e-8)  # adagrad update
+
+        #print loss for batch
+        print('Loss: %d'%loss)
+
+        #see if next batch exists
+        start_index = start_index + _batch_size
+        end_index = end_index + _batch_size
+        if start_index >= len(input_text):
+            cont = False
+        elif end_index > len(input_text):
+            end_index = len(input_text)
+
+    return W_xh, W_hh, W_hy, b, b_prime
 
 
-
-
-        #save for next iter
 
 
 
@@ -120,6 +166,7 @@ def parse_arguments():
     arg_parser.add_argument('--f', type=str, dest='input_file')
     arg_parser.add_argument('--b', type=int, dest='batch_size')
     arg_parser.add_argument('--h', type=int, dest='hidden_layer_size')
+    arg_parser.add_argument('-lr', type=int, dest='learning_rate')
 
     return arg_parser.parse_args()
 
@@ -128,7 +175,7 @@ if __name__ == 'main':
 
     #parse arguments
     arguments = parse_arguments()
-    main(arguments.input_file, arguments.batch_size, arguments.hidden_layer_size)
+    main(arguments.input_file, arguments.batch_size, arguments.hidden_layer_size, arguments.learning_rate)
 
 
 
